1ba2a8147317c98c2ec50717484bc3de1351465e
Gabriel Renostro <gabrielrenostro581@gmail.com>
2025-04-09 16:08:19 -0300

    hehe

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -173,7 +173,7 @@
     ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: rec {
       src = (builtins.fetchTarball {
         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-        sha256 = "sha256:0s4ijhrjfrz9chbi5mdas7g7lvvcw6r0r25nhzckvffnpc999a64";
+        sha256 = "sha256:1ys4d79pnf1y4aslq0bnm20cgmmfcgl7cd3lhw88h41mx2bi1hfl";
       });
       version = "latest";
       buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
